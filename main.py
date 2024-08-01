d84f1d3920cd198c5037f9a90995b0f09449790c
Nishahtc <nishamodi254@email.com>
2024-08-01 16:04:41 +0530

    packag

---
+++ main.py
@@ -0,0 +1,8 @@
+from main_package.sub_package1 import submodule1
+submodule1.fun1()
+from main_package .sub_package1 import submodule2
+submodule2.fun2()
+from main_package import module1
+module1.name()
+from main_package import modules2
+modules2.names()
---
+++ main_package/modules2.py
@@ -0,0 +1,2 @@
+def names():
+    print("This is file")
